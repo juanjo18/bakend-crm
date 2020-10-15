96649aa4cc32f996a18e40237bb427335bf2d62c
juanjo18 <juanjo-barranky@hotmail.com>
2020-10-15 14:23:38 -0500

    Ruta Usuario lista

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -59,4 +59,66 @@ app.post('/',  (req, res)=>{
     })
 });
 
+app.put('/:id', (req, res) =>{
+    var id = req.params.id;
+    var body = req.body;
+    
+    Usuario.update(
+        {
+            nombre: body.nombre,
+            email: body.email
+        },
+
+        { where: {
+            id_usuario : id
+        }}
+    )
+    .then(resultado => {
+        if(resultado){
+            res.status(200).json({
+                ok: 'true',
+                mensaje: 'Usuario actualizado'
+            })
+        }
+        else{
+            return res.status(400).json({
+                ok: 'false',
+                mensaje: 'No es encuentra ese usuario'
+            })
+        }
+    })
+    .catch(err =>{
+        return res.status(500).json({
+            ok: 'false',
+            mensaje: 'Error al actualizaar el usuario'
+        })
+    })
+});
+
+// ==========================================
+//  Delete an user
+// ==========================================
+app.delete('/:id', (req, res) =>{
+    var id = req.params.id;
+
+    Usuario.destroy({
+        where: {
+            id_usuario: id
+        }
+    })
+    .then(resultado => {
+        res.status(200).json({
+            ok: 'true',
+            mensaje: 'Usuario Eliminado'
+        })
+    })
+    .catch(err => {
+        return res.status(500).json({
+            ok: 'false',
+            mensaje: 'Error al eliminar el usuario',
+            error: err
+        })
+    })
+});
+
 module.exports = app;
